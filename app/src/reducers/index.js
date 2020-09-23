db5ebc136d5af66eb5cf3ecefa71ece2f9620ba0
angelynaliem <angelynaliem@gmail.com>
2020-09-22 22:10:47 -0400

    Created reducer

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,3 +1,6 @@
+import * as actions from "../actions";
+
+
 const initialState = {
 
     quote: "Bible quote of the day",
@@ -5,3 +8,34 @@ const initialState = {
     error: ""
 
 }
+
+export default reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+
+        case actions.QUOTE_FETCHING:
+            console.log("fetching a quote")
+            return {
+                ...state,
+                isFetching: true
+            }
+            case actions.QUOTE_FETCHED:
+                console.log("fetching success")
+                return {
+                    ...state,
+                    quote: action.payload,
+                    isFetching: false
+                }
+            case actions.QUOTE_ERROR:
+                console.log("fetching failed")
+                return {
+                    ...state,
+                    error: action.payload,
+                    isFetching: false
+                }
+            default:
+                return state
+
+    }
+
+} 
